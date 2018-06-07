9009d00d168164dedf7e972731562cee76afeb58
Michal <michalhz159@gmail.com>
2018-06-07 14:30:33 +0200

    Initial commit - updated readme - soution setup - simple file downloading

---
+++ BillScanner/Program.cs
@@ -0,0 +1,74 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Linq;
+
+namespace BillScanner {
+	class Program {
+		private static string folderPath { get; } = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar;
+		private static string billPath { get; } = folderPath + "Bills" + Path.DirectorySeparatorChar;
+
+		static void Main(string[] args) {
+
+			if (!Directory.Exists(billPath)) {
+				Directory.CreateDirectory(billPath);
+			}
+
+			UserCredential credential;
+
+			using (FileStream stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read)) {
+				string credPath = Path.Combine(folderPath, "drive-dotnet-quickstart.json");
+
+				credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+					GoogleClientSecrets.Load(stream).Secrets,
+					new string[1] { DriveService.Scope.Drive },
+					"user",
+					CancellationToken.None,
+					new FileDataStore(folderPath, true)).Result;
+				Console.WriteLine("Credential file saved to: " + credPath);
+			}
+
+			// Create Drive API service.
+			DriveService service = new DriveService(new BaseClientService.Initializer() { HttpClientInitializer = credential, ApplicationName = "Bill Scanner" });
+
+			FilesResource.ListRequest listRequest = service.Files.List();
+			listRequest.Q = "mimeType != 'application/vnd.google-apps.folder' and name contains 'Dokument aplikace' and name contains 'Google Keep'";
+
+
+			IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute().Files;
+
+			Console.WriteLine("Files:");
+			if (files != null && files.Count > 0) {
+				foreach (Google.Apis.Drive.v3.Data.File file in files) {
+					Console.WriteLine("{0} ({1})", file.Name, file.Id);
+					if (file.Name.Contains("Google") || file.Name.Contains("Keep")) {
+						Stream fs;
+						try {
+							fs = new FileStream(billPath + file.Name + "-" + file.Id.Replace("-", "") + ".txt", FileMode.CreateNew);
+						}
+						catch (IOException e) {
+							Console.WriteLine(e.Message);
+							continue;
+						}
+						service.Files.Export(file.Id, "text/plain").DownloadWithStatus(fs);
+						fs.Close();
+						fs.Dispose();
+						//TODO delete file from drive
+					}
+				}
+			}
+			else {
+				Console.WriteLine("No files found.");
+			}
+			Console.WriteLine("All actions finished\n" +
+							  "Press 'Enter' to exit...");
+			Console.ReadLine();
+		}
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # BillScanner
+**WARNING: This is WIP, not ready for use!**
+
 Fetch OCR'd shopping bills from GDrive and get interesting information
+
+## The plan
+
+With this program you will be able to fetch documents from Google Drive that contain OCR text from an image that was taken by your smartphone.\
+
+#### Currently the best way I found is to:
+##### With the Keep App
+---
+With your Smartphone
+* Download [Google Keep](https://play.google.com/store/apps/details?id=com.google.android.keep&hl=en), [(IOS)](https://itunes.apple.com/us/app/google-keep/id1029207872?mt=8)
+* Take a picture of the bill
+---
+At Home
+* Open Google keep from browser (The app doesnt allow OCR -.-)
+* OCR the bill and move it to Google Drive
+* Run this program
+
+##### Wthout Keep App
+---
+With your Smartphone
+* Go to Google Keep and select "**_Show Desktop version_**"
+* Take a picture of the bill
+* OCR the bill and move it to Google Drive
+---
+At Home
+* Run this program
