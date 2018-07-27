e5e487a7b46a961eed70cac37757d70a7badafc9
Michal <michalhz159@gmail.com>
2018-07-27 12:50:35 +0200

    Initial Unity commit - added first iteration button - connection info

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+class ButtonBehaviour : MonoBehaviour {
+	public RawImage image;
+	public InputField amount;
+	public Button button;
+	public Button removeItem;
+
+	public Main.MeassurementUnit unit;
+
+	private void Start() {
+		amount.onEndEdit.AddListener(OnInputFieldTextEntered);
+		switch (unit) {
+			case Main.MeassurementUnit.PIECES:
+			amount.contentType = InputField.ContentType.IntegerNumber;
+			break;
+			case Main.MeassurementUnit.WEIGHT:
+			amount.contentType = InputField.ContentType.DecimalNumber;
+			break;
+			case Main.MeassurementUnit.LITRES:
+			amount.contentType = InputField.ContentType.IntegerNumber;
+			break;
+		}
+	}
+
+	private void OnInputFieldTextEntered(string value) {
+		button.image.color = new Color(0.5f, 1f, 0.6f, 0.8f);
+		removeItem.gameObject.SetActive(true);
+	}
+
+	public void OnButtonPress() {
+		button.image.color = new Color(0.5f, 1f, 0.6f, 0.8f);
+		EventSystem.current.SetSelectedGameObject(amount.gameObject);
+		removeItem.gameObject.SetActive(true);
+		amount.text = "1";
+	}
+
+
+
+	public void OnRemoveButton() {
+		button.image.color = new Color32(255, 255, 255, 100);
+		removeItem.gameObject.SetActive(false);
+		amount.text = "";
+	}
+}
+
--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,57 +1,32 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
 
 public class Main : MonoBehaviour {
 
+	public enum MeassurementUnit {
+		PIECES,
+		WEIGHT,
+		LITRES
+	}
+
+	public static Text getText { get; private set; }
+	public ConnectionManager conn;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		getText = transform.Find("Text").GetComponent<Text>();
+		conn = new ConnectionManager();
+		conn.prefab = Resources.Load<GameObject>("ConnInfo");
+		conn.ConnectInfo();
 	}
 
-	static void Main(string[] args) {
-		Console.o
-		Console.WriteLine("Client");
-		Console.WriteLine("Enter host address");
-		//IPAddress addr;
-		//using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0)) {
-		//	socket.Connect("8.8.8.8", 65530);
-		//	IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
-		//	addr = endPoint.Address;
-		//}
-		byte[] bytes = new byte[4086];
-		TcpClient c = new TcpClient();
-		c.Connect(IPAddress.Parse(Console.ReadLine()), 7890);
-		Console.WriteLine("Connection established");
-		NetworkStream ns = c.GetStream();
-		IAsyncResult ress = ns.BeginRead(bytes, 0, bytes.Length, OnReceived, ":ok_hand:");
-		evnt.Wait();
-		int data_count = ns.EndRead(ress);
-		evnt.Reset();
-
-		Console.WriteLine(System.Text.Encoding.Default.GetString(SubArray(bytes, 0, data_count)));
-		Console.ReadLine();
-
-		return;
-	}
-
-	static ManualResetEventSlim evnt = new ManualResetEventSlim();
-
-	private static void OnReceived(IAsyncResult ar) {
-		Console.WriteLine("Got some data!");
-		evnt.Set();
-
-	}
-
-	public static T[] SubArray<T>(T[] data, int index, int length) {
-		T[] result = new T[length];
-		Array.Copy(data, index, result, 0, length);
-		return result;
+	public void Send() {
+		conn.SendData();
 	}
 }
